8d1d933a548addfaaed011bd2cac545321ad4483
arnaud-eb <arnaud.depierreux@easebest.com>
2022-02-17 10:23:01 +0100

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import styled from "styled-components";
+
+import Reviews from "./components/Reviews";
+
+const AppWrapper = styled.main`
+  width: 90vw;
+  margin: 5rem auto;
+  max-width: var(--max-width);
+
+  @media screen and (min-width: 992px) {
+    width: 95vw;
+  }
+`;
+
+const Title = styled.div`
+  text-align: center;
+  margin-bottom: 2rem;
+
+  h2 {
+    font-weight: 500;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    span {
+      font-size: 0.85em;
+      color: var(--clr-primary-5);
+      margin-right: 1rem;
+      font-weight: 700;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppWrapper>
+      <Title>
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </Title>
+      <Reviews />
+    </AppWrapper>
   );
 }
 
